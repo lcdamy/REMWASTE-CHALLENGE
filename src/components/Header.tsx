bb8a93cebec3cfaeffa2abd951ecfdfcbaefea47
lcdamy <zudanga@gmail.com>
2025-06-05 14:47:45 +0200

    fix: Update text color in Header and SubHeader components for better visibility

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { CiSun } from "react-icons/ci";
 const Header = () => {
     return (
         <div className="flex justify-center md:justify-end mt-[15px] ">
-            <h1 className="border border-white px-4 py-2 mt-2 dark:text-white rounded-md flex items-center gap-2">
+            <h1 className="border border-white px-4 py-2 mt-2 text-white dark:text-white rounded-md flex items-center gap-2">
                 <CiSun /> Switch to light mode
             </h1>
         </div>
--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,7 +1,7 @@
 const SubHeader = () => {
   return (
         <div className="text-center">
-            <h1 className="font-[600] text-[24px] mb-2 dark:text-white">Choose your skip size</h1>
+            <h1 className="font-[600] text-[24px] mb-2 text-white dark:text-white">Choose your skip size</h1>
             <h1 className="text-[14px] text-white/70">Select the skip size that best suit your needs</h1>
         </div>
   );
